92336ee682cbe3d144051024f328073ab6b23e0e
adam5096 <adamchou5096@gmail.com>
2024-04-12 16:48:48 +0800

    Creating Blog Pages

--- app.vue
+++ app.vue
@@ -3,14 +3,18 @@
     <!-- Header -->
     <Header class="mt-8 mb-4 flex justify-between items-end">
       <div>
-        <h1 class="text-2xl font-semibold">personal Blog</h1>
+        <h1 class="text-2xl font-semibold">
+          <!-- 跳轉至首頁/ -->
+          <NuxtLink to="/">Personal Blog</NuxtLink>
+        </h1>
         <p class="text-gray-500">Learn programming easily</p>
       </div>
 
-      <contentBtn>Contact/Hire me</contentBtn>
+      <Btn>Contact/Hire me</Btn>
     </Header>
 
     <!-- Main content -->
+    <!-- 以pages目錄內的index.vue 為主頁-->
     <NuxtPage />
   </div>
 </template>
---
+++ pages/[slug].vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+// 取得Url中name參數，寫法一
+// const route = useRoute();
+// const blabla = route.params.name
+
+// 取得Url中name參數，寫法二
+const { slug } = useRoute().params;
+</script>
+
+<template>
+    <div>
+        {{ slug }}
+    </div>
+</template>
+
+
+
+<style></style>
