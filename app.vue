d83eae536e6887ed04acdb1c122b815f48fb38ff
adam5096 <adamchou5096@gmail.com>
2024-04-12 16:29:35 +0800

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="max-w-3xl px-4 mx-auto">
+    <!-- Header -->
+    <Header class="mt-8 mb-4 flex justify-between items-end">
+      <div>
+        <h1 class="text-2xl font-semibold">personal Blog</h1>
+        <p class="text-gray-500">Learn programming easily</p>
+      </div>
+    </Header>
+  </div>
+</template>
